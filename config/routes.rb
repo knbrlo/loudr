86ea812c02db6378756f5ef965f5229aaa25f81e
Ken Barlow <kenbrlow@gmail.com>
2021-02-09 10:13:49 -0700

    Updated readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  
+
   # TODO - come back and make sure to remove any routes that you're not using.
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

9eef0476de685d25f4786482c7b2a7686eb6e469
Ken Barlow <kenbrlow@gmail.com>
2021-02-10 14:33:05 -0700

    Setup validations on the user model for username, email presence and uniqueness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+
+    # this is taking care of password validations
     has_secure_password
+
+    # more validations for user attributes
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end

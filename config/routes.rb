5ba981b78a9fc90815a27336ffa2814bdcd63d77
Ken Barlow <kenbrlow@gmail.com>
2021-02-10 14:23:39 -0700

    Added some new routes for home and login. Also added a controller for sessions and a view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
-  
+  # root
+  root 'sessions#home'
+
+  # sign up
+  get '/signup' => 'users#new' 
+
+  # login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 
   # TODO - come back and make sure to remove any routes that you're not using.
   resources :users

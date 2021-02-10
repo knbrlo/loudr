b722a74a145640b5b4bf2afb20c7a162ad6d76aa
Ken Barlow <kenbrlow@gmail.com>
2021-02-10 14:29:59 -0700

    Built sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+    # displaying the sign up form
+    def new
+        # make a new user that we can access within our form
+        @user = User.new
+    end
+
 end

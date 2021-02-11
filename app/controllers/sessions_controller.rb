ef99996f77e9b1a8274c83b7c669577723ffcd60
Ken Barlow <kenbrlow@gmail.com>
2021-02-11 09:31:46 -0700

    Confirmed signing in and out works for users and creators.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,20 @@ class SessionsController < ApplicationController
 
     def new
     end
+
+    # logout - user
+    # todo - these are setup as different paths for user and creator so we can take different actions for each.
+    def destroyuser
+        session.clear
+        redirect_to '/'
+    end
+
+
+    # logout - creator
+    # todo - these are setup as different paths for user and creator so we can take different actions for each.
+    def destroycreator
+        session.clear
+        redirect_to '/'
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,20 +11,25 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
-  # home - user
-  get '/user-home' => 'users#home' 
-
-  # home - creator
-  get '/creator-home' => 'creators#home' 
+  # user
+  get '/user-home' => 'users#home'
+  delete '/user-logout' => 'sessions#destroyuser'
 
   # TODO - come back and make sure to remove any routes that you're not using.
   resources :users
 
-  # TODO - come back and make sure to remove any routes that you're not using.
-  resources :contents
+  # creator
+  get '/creator-home' => 'creators#home' 
+  delete '/creator-logout' => 'sessions#destroycreator'
 
   # TODO - come back and make sure to remove any routes that you're not using.
   resources :creators
+
+  # contents
+  # TODO - come back and make sure to remove any routes that you're not using.
+  resources :contents
+
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

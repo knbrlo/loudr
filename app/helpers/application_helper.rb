551c28ab2c207bee389346e4936f646d6632534b
Ken Barlow <kenbrlow@gmail.com>
2021-02-11 09:15:27 -0700

    Added some methods to help with determining the logged in user / creator. Also some methods to verify session status for both user and creator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+    def current_user
+        # todo - what is meant by memoization here? is that the right term?
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def current_creator
+        # todo - what is meant by memoization here? is that the right term?
+        @current_creator ||= Creator.find_by_id(session[:creator_id])
+    end
+
+    def logged_in_user?
+        !!session[:user_id]
+    end
+
+    def logged_in_creator?
+        !!session[:creator_id]
+    end
 end

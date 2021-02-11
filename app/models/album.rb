bce8913ffc08e2eefb5307f41361938cab3eb1c3
Ken Barlow <kenbrlow@gmail.com>
2021-02-11 14:25:47 -0700

    Setup some initial relationships for albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,12 @@
 class Album < ApplicationRecord
+
+    # relationships
+    belongs_to :creator
+    belongs_to :user
+
+    # validations
+
+
+    # todo - create a migration to store images for each album
+
 end

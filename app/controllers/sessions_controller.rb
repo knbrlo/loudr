374105b5b656e11f997f62a88f1b54079f061250
Ken Barlow <kenbrlow@gmail.com>
2021-02-11 13:10:47 -0700

    Finished login pages for user and creator

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,33 @@ class SessionsController < ApplicationController
     def home
     end
 
+    # this creates a new user that we can use to login with
+    # and then we use that user to login using the 'create' path below.
     def new
         @user = User.new
         render :login
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        
+        # check if the user exists and if we can login
+        if @user && @user.authenticate(params[:user][:password])
+
+            # set the session user_id
+            session[:user_id] = @user.id
+
+            # send the user to their home page
+            redirect_to home_path
+        else
+            # if the user wasn't found / or we're unable to login
+            # then display a generic error, we don't want to tell them
+            # if the user exists if this is a malicious attempt.
+            flash[:error] = "Unable to login, please check your username or passsword"
+
+            # we redirect so that nothing is saved when we display errors.
+            redirect_to '/login'
+        end
     end
 
     def newcreator
@@ -17,6 +38,25 @@ class SessionsController < ApplicationController
     end
 
     def createcreator
+        @creator = Creator.find_by(username: params[:creator][:username])
+        
+        # check if the creator exists and if we can login
+        if @creator && @creator.authenticate(params[:creator][:password])
+
+            # set the session creator_id
+            session[:creator_id] = @creator.id
+
+            # send the creator to their home page
+            redirect_to home_creator_path
+        else
+            # if the creator wasn't found / or we're unable to login
+            # then display a generic error, we don't want to tell them
+            # if the creator exists if this is a malicious attempt.
+            flash[:error] = "Unable to login, please check your username or passsword"
+
+            # we redirect so that nothing is saved when we display errors.
+            redirect_to '/login-creator'
+        end
     end
 
 

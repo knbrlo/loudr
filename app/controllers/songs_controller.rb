ba207337d68850dfa233a3c80e9005b5716c488b
Ken Barlow <kenbrlow@gmail.com>
2021-02-11 17:37:07 -0700

    Added ability to show all the songs tied to an album on the album show page.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,7 +9,30 @@ class SongsController < ApplicationController
 
     def create
         puts "make a new song for this album"
-        puts params
+        puts params[:album_id]
+        @album = Album.find_by_id(params[:album_id])
+
+        puts "album is:"
+        puts @album
+
+        @new_song = @album.songs.build(song_params)
+
+        if @new_song.save
+            p 'New song is '
+            p @new_song
+
+            # todo - reroute back to the album show page and show all the songs for the album
+            redirect_to album_path(@album)
+        else
+            p 'Didnt make new song'
+        end
+        
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:name, :duration, :song_number, :explicit)
     end
 
 end

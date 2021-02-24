a16af78ac6bf2b73387d2d670c2f7ecb8acbedc6
Ken Barlow <kenbrlow@gmail.com>
2021-02-23 18:26:33 -0700

    Removed comments from creators controller.

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,58 +1,34 @@
 class CreatorsController < ApplicationController
 
     def new
-        # make a new creator user that we can access within our form.
         @creator = Creator.new
     end
 
     def create
-
-        # create the creator from the params from the form.
         @creator = Creator.new(creator_params)
-
-        # if the creator is successfully saved then set the sessions id
-        # then redirect to the new page.
         if @creator.save
-
-            # save the creator id in the session so it can be accessed anywhere it's needed.
             session[:creator_id] = @creator.id
-
-            # send the user to the path for our creator home page
             redirect_to home_creator_path
         else
-            # send the user back to thew new page to create a new creator
-            # also this helps us display flash messages and makes so we don't
-            # keep any saved information around such as username / email / password
             render :new
         end
     end
 
     def home
-
-        # make sure the creator is logged in to see the home-creator path
         if logged_in_creator?
 
-            # make empty arrays we can check for values
-            # and iterate over if there are values in the view.
             @singles = []
             @albums = []
             @podcasts = []
 
-
-            # if there are singles in available then assign  
-            # them to the array we created above
             if singles = Single.where(creator_id: session[:creator_id])
                 @singles = singles
             end
 
-            # if there are albums in available then assign  
-            # them to the array we created above
             if albums = Album.where(creator_id: session[:creator_id])
                 @albums = albums
             end
 
-            # if there are albums in available then assign  
-            # them to the array we created above
             if podcasts = Podcast.where(creator_id: session[:creator_id])
                 @podcasts = podcasts
             end

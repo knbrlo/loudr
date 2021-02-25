8de63a4ca6901fe10b68aa6691ff1a526faf47ec
Ken Barlow <kenbrlow@gmail.com>
2021-02-25 14:36:40 -0700

    Added email validation for creators.

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -10,4 +10,5 @@ class Creator < ApplicationRecord
     has_secure_password
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@ class User < ApplicationRecord
 
     def self.from_omniauth(auth)
         where(email: auth.info.email).first_or_initialize do |user|
+          user.password = SecureRandom.hex
           user.username = auth.info.name
           user.email = auth.info.email
-          user.password = SecureRandom.hex
         end
     end
 end
